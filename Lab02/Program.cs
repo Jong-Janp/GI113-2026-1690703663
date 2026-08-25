a5ceec41c62d291d8b22cd1e3087822fee790360
Jong-Janp <jongngwon.janp@bumail.net>
2026-08-25 17:21:51 +0700

    Update Program.cs

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -39,6 +39,32 @@ namespace Lab02
             hpPercent = currentHp * 100 / maxHp;
             Console.WriteLine($"HP Precent: {hpPercent}%");
 
+            string charName = "Nou";
+            char speed = 'A';
+            int maxAP = 1000;
+            int currentAP = 240;
+            int range = 175;
+            float attackSpeed = 0.6f;
+            double critChance = 20;
+            bool isChar = true;
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {charName}");
+            Console.WriteLine($"Speed: {speed}");
+            Console.WriteLine($"MaxAP: {maxAP}");
+            Console.WriteLine($"AP: {currentAP}");
+            Console.WriteLine($"Range: {range}");
+            Console.WriteLine($"Critchance: {critChance}");
+            Console.WriteLine($"Isboss: {isChar}");
+            Console.WriteLine();
+            int APPercent = currentAP * 100 / maxAP;
+            Console.WriteLine($"AP percent: {APPercent}%");
+            Console.WriteLine();
+            currentAP = currentAP * 60;
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: AFTER DAMAGE =====");
+            Console.WriteLine($"AP: {currentAP} / {maxAP}");
+            APPercent = currentAP * 100 / maxAP;
+            Console.WriteLine($"AP Precent: {APPercent}%");
 
         }
     }
